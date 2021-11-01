27061552c9a4b2598cae486a2069b57cf2b37f02
Micah Lindley <micahincorporated@gmail.com>
2021-11-01 14:38:08 -0500

    Fixed Heroku port binding

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const puppeteer = require('puppeteer');
 const express = require('express');
 const app = express();
 var path = require('path');
-
+const port = process.env.PORT || 8080;
 var areResultsReady = false;
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+  const browser = await puppeteer.launch({ headless: true, defaultViewport: null, args: ['--no-sandbox', '--disable-setuid-sandbox', "--incognito", "--single-process", "--no-zygote"] });
   const page = await browser.newPage();
   await page.setRequestInterception(true);
   page.on('request', (request) => {
@@ -24,7 +24,7 @@ var areResultsReady = false;
     } else {
       let first = await page.evaluate(() => window.find("2021"));
       let second = await page.evaluate(() => window.find("2021"));
-      if (first) {
+      if (first && second) {
         // then results are posted
         loop = false;
         areResultsReady = true;
@@ -46,6 +46,6 @@ app.get('/results', (req, res) => {
   res.send(areResultsReady);
 })
 
-app.listen(8080, () => {
+app.listen(port, () => {
   console.log(`Example app listening`);
 });
